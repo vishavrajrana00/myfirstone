70ce18eab4de5ed58f91be18a248031f1a61cf4d
vishavraj rana <vishavraj.rana@fosteringlinux.com>
2025-09-15 13:06:00 +0530

    latest deployment code

---
+++ devops/backend/main.py
@@ -0,0 +1,22 @@
+# main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow all origins (not secure for production!)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to FastAPI! This is 1st deployment"}
+
+@app.get("/hello/{name}")
+def hello(name: str):
+    return {"message": f"Hello, {name}!"}
